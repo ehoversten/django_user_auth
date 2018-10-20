b5c82e3bccd0124563d76fd407e27da6b87c9494
ehoversten <ehoversten@gmail.com>
2018-10-20 18:38:04 -0400

    git upload

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -32,6 +32,18 @@ def signup_page(request):
 
 def login_page(request):
 
+    # username = request.POST['username']
+    # password = request.POST['password']
+    # user = authenticate(username=username, password=password)
+    # if user is not None:
+    #     if user.is_active:
+    #         login(request, user)
+    #         # Redirect to a success page.
+    #     else:
+    #         # Return a 'disabled account' error message
+    # else:
+    #     # Return an 'invalid login' error message.
+
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
